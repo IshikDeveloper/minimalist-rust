a2f6d30d1ac98115a7391b57dc3ef0d090497eeb
Adam <adam453345@gmail.com>
2025-11-21 08:06:28 +0300

    guitar

---
+++ src/assets/mod.rs
@@ -0,0 +1,5 @@
+// src/assets/mod.rs
+// Placeholder for browser pages module
+pub mod browser_pages;
+
+pub use browser_pages::BROWSER_PAGES;
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,66 @@
-// main.rs - Application entry point
-use std::env;
+// main.rs - Fixed version using wry properly
 use std::path::PathBuf;
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
 };
 use log::info;
 
 mod browser_core;
-mod main_window;
 mod assets;
 
 use browser_core::BrowserCore;
-use main_window::MinimalistWindow;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize logger
+fn main() -> wry::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     info!("Starting Minimalist Browser v1.0.0");
 
-    // Set up paths
-    setup_browser_directories()?;
-    
-    // Check for Flash support
+    setup_browser_directories().expect("Failed to setup directories");
     check_flash_installation();
 
-    // Create event loop
     let event_loop = EventLoop::new();
-    let mut browser = BrowserCore::new();
-    let window = MinimalistWindow::new(&event_loop);
+    let window = WindowBuilder::new()
+        .with_title("Minimalist Browser")
+        .with_inner_size(wry::application::dpi::LogicalSize::new(1280.0, 850.0))
+        .build(&event_loop)?;
 
-    // Run the browser
+    let webview = WebViewBuilder::new(window)?
+        .with_url("minimalist://newtab")?
+        .with_custom_protocol("minimalist", |request| {
+            let path = request.uri().path();
+            let (content, mime_type) = match path {
+                "/newtab" => ("New Tab Page", "text/html"),
+                "/settings" => ("Settings Page", "text/html"),
+                "/memory" => ("Memory Stats", "text/html"),
+                "/flash" => ("Flash Test", "text/html"),
+                _ => ("Not Found", "text/plain"),
+            };
+            Ok(wry::http::Response::builder()
+                .header("Content-Type", mime_type)
+                .body(content.as_bytes().to_vec())?)
+        })
+        .build()?;
+
+    let mut browser = BrowserCore::new();
+    browser.create_tab("minimalist://newtab");
+
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
+        *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => {
-                    info!("Shutting down browser...");
-                    *control_flow = ControlFlow::Exit;
-                }
-                WindowEvent::KeyboardInput { input, .. } => {
-                    browser.handle_keyboard_input(input, window.get_modifiers());
-                }
-                _ => {}
-            },
-            Event::MainEventsCleared => {
-                // Update UI
-                window.window.request_redraw();
+            Event::NewEvents(StartCause::Init) => {
+                info!("Browser initialized");
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                info!("Closing browser");
+                *control_flow = ControlFlow::Exit;
             }
             _ => {}
         }
@@ -72,7 +85,7 @@ fn setup_browser_directories() -> std::io::Result<()> {
 fn check_flash_installation() {
     let flash_path = PathBuf::from("plugins/pepflashplayer32_32_0_0_465.dll");
     if !flash_path.exists() {
-        info!("Flash plugin not found. Run setup script to download.");
+        info!("Flash plugin not found. Browser will use Ruffle emulation.");
     } else {
         info!("Flash plugin detected at: {:?}", flash_path);
     }
